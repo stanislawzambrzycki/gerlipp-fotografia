dc2fd38490f523fab1f9304675482cbf037bf65c
stanislawzambrzycki <stanislaw.zambrzycki@poczta.onet.pl>
2020-11-05 12:28:25 +0100

    scss + gallery folder

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <v-main class="background" style="height: 200vh">
-        <v-btn fixed top right icon fab><v-icon>menu</v-icon></v-btn>
-        <Greeting />
-        <Galleries />
-        <About />
-        <Contact />
+      <v-btn fixed top right icon fab><v-icon>menu</v-icon></v-btn>
+      <Greeting />
+      <Galleries />
+      <About />
+      <Contact />
     </v-main>
   </v-app>
 </template>
 
-<script src="./App.js"/>
-<style src="./css/main.css"/>
+<script src="./App.js" />
+<style src="../public/css/main.css" />
--- src/components/galleries/galleries.js
+++ src/components/galleries/galleries.js
@@ -1,14 +1,17 @@
-import Gallery from "./gallery/gallery.vue"
+import Gallery from "./gallery/gallery.vue";
 
 export default {
-    name: 'Galleries',
-    components: {
-        Gallery
-    },
-    data() {
-        return {
-            tab: null,
-            galleries: [{name: "portrety", images: [1, 2, 3, 4]}, {name: "podróże", images: [1, 2, 3]}]
-        }
-    }
-}
+  name: "Galleries",
+  components: {
+    Gallery,
+  },
+  data() {
+    return {
+      tab: null,
+      galleries: [
+        { name: "portrety", images: [1, 2, 3, 4] },
+        { name: "podróże", images: [1, 2, 3] },
+      ],
+    };
+  },
+};
