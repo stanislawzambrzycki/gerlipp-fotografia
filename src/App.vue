a8d4092b76472508cd1ceb07e88c96aef11e6fc4
stanislawzambrzycki <stanislaw.zambrzycki@poczta.onet.pl>
2020-11-14 03:22:27 +0100

    responsywność w greeting, menu, about i contact, bez dialogu. Tymczasowo ustawiłem obrazek w about na display:none, wydawał się psuc marginesy. Ustawiłem hovery dla obrazków i pozycji w menu, dałem focusowi outline: none !important; vuetify wymuszał te outliny w kilku miejscach, pozostałe focusy zostawiłem. paralax w greeting działa tylko na duzych i bardzo duzych ekranach, tam gdzie będzie dobrze wyglądał z tym obrazkiem. Na mniejszych ekranach mamy ładny widoczny obrazek.

--- src/App.vue
+++ src/App.vue
@@ -24,3 +24,12 @@
 
 <script src="./App.js" />
 <style src="../public/css/main.css" />
+
+/* 1. do poprawienia w przyszłości z-indexy na ikonach i menu, na potrzeby
+prezentacji myślę, że ok 2. parallax działa tylko na dużych i bardzo duzych
+ekranach, na pozostałych mamy usmiechniętą buzię 3. obrazek z about na razie ma
+"display: none", wydawał się w jakiś sposob kolidować z Twoją galerią (tworzył
+jej duzy margines po prawej strtonie, chociaż bez niego tez jest krótki moment
+gdy sie pojawia) 4. w menu zająłem się responsywnością, ale zostawiłem Ci js-a,
+padam na twarz. 5. dialog ma problemy z responsywnością na małych ekranachale
+nie chciałem w nim grzebać bez ciebie */
--- src/components/greeting/greeting.vue
+++ src/components/greeting/greeting.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="greeting">
     <v-parallax
-      height="700"
+      height="1000"
       src="https://static.wixstatic.com/media/9579ed_34a75abadc2a497ab79eca9ee41d2f61~mv2.jpg/v1/fill/w_1920,h_1920,al_c,q_85,usm_0.66_1.00_0.01/9579ed_34a75abadc2a497ab79eca9ee41d2f61~mv2.webp"
     >
       <div class="hello">
