918cfcf737decb292c69846f9a8b099113d24aa1
stanislawzambrzycki <stanislaw.zambrzycki@poczta.onet.pl>
2021-01-03 15:17:06 +0100

    contat db is working

--- src/components/cms/settings/about_cms.js
+++ src/components/cms/settings/about_cms.js
@@ -1,25 +1,43 @@
-import firebase from "firebase"
-import "firebase/firestore"
-import 'firebase/app'
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/app";
 
 export default {
-    name: "aboutCMS",
-    created() {
-        this.db.collection('about').get().then(aboutCollection => {
-            this.aboutRef = aboutCollection.docs[0].ref
-            this.aboutText = aboutCollection.docs[0].data().text
-        })
+  name: "aboutCMS",
+  created() {
+    this.db
+      .collection("about")
+      .get()
+      .then((aboutCollection) => {
+        this.aboutRef = aboutCollection.docs[0].ref;
+        this.aboutText = aboutCollection.docs[0].data().text;
+      });
+
+    this.db
+      .collection("contact")
+      .get()
+      .then((contactCollection) => {
+        this.contactRef = contactCollection.docs[0].ref;
+        this.contactText = contactCollection.docs[0].data();
+      });
+  },
+
+  data() {
+    return {
+      db: firebase.firestore(),
+      aboutText: "",
+      aboutRef: null,
+      contactText: "",
+      contactRef: [],
+    };
+  },
+  methods: {
+    saveAbout() {
+      this.aboutRef.update({ text: this.aboutText });
     },
-    data() {
-        return {
-            db: firebase.firestore(),
-            aboutText: "",
-            aboutRef: null
-        }
+    saveContact() {
+      this.aboutRef.update({ text: this.aboutText.phone });
+      this.aboutRef.update({ text: this.aboutText.email });
     },
-    methods: {
-        saveAbout() {
-            this.aboutRef.update({text: this.aboutText})
-        }
-    },
+  },
 };
--- src/components/contact/contact.vue
+++ src/components/contact/contact.vue
@@ -3,12 +3,12 @@
     <div class="text-content">
       <div class="header">Contact</div>
       <div class="p">
-        <v-icon large color="primary" class="mr-5">mdi-at</v-icon>
-        {{ contactText }}
+        <v-icon large color="primary" class="mr-5">mdi-phone-outline</v-icon>+48
+        {{ contactText.phone }}
       </div>
       <div class="p">
-        <v-icon large color="primary" class="mr-5">mdi-phone-outline</v-icon>+48
-        {{ contactText }}
+        <v-icon large color="primary" class="mr-5">mdi-at</v-icon>
+        {{ contactText.email }}
       </div>
     </div>
   </v-card>
