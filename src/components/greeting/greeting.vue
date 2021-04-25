750bc76d93b92c3b11fa211256d115f819346e4f
Zbigniew Zambrzycki <spamherenoobs@gmail.com>
2021-04-25 07:14:41 +0200

    changes omg

--- src/components/greeting/greeting.vue
+++ src/components/greeting/greeting.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="greeting" data-name="greeting">
-    <v-parallax
-      :height="windowHeight"
-      :src="greetingText.display? greetingText['image'+imageNumber]: null"
-    >
-      <div class="hello">
-        <h1>
-          <span>{{ greetingText.top }}</span>
-        </h1>
-        <h1>{{ greetingText.center }}</h1>
+  <v-responsive :aspect-ratio="1 / 1" class="greeting" data-name="greeting">
+    <v-img
+      class="greeting-image scroll"
+      data-rate="0.25"
+      :src="greetingText.display ? greetingText.image : null"
+    />
+    <div class="hello">
+      <h1>
+        <span>{{ greetingText.top }}</span>
+      </h1>
+      <h1>{{ greetingText.center }}</h1>
 
-        <div>
-          <h5>{{ greetingText.bottom }}</h5>
-          <v-btn icon dark large @click="$emit('navigate', 'showcase')"
-            ><v-icon large>expand_more</v-icon></v-btn
-          >
-        </div>
+      <div>
+        <h5>{{ greetingText.bottom }}</h5>
+        <v-btn icon dark large @click="$emit('navigate', 'showcase')"
+          ><v-icon large>expand_more</v-icon></v-btn
+        >
       </div>
-    </v-parallax>
-  </div>
+    </div>
+  </v-responsive>
 </template>
 <script src="./greeting.js" />
